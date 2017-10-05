522182d90ed72bf93ba0a1dc2923fb454a47d811
Lich <lichbanned@gmail.com>
2017-10-05 14:31:55 +0500

    fix redis

--- s3-to-gs.go
+++ s3-to-gs.go
@@ -31,6 +31,110 @@ type MyWork struct {
 	count        *int
 }
 
+func main() {
+	s3endpoint := flag.String("s3endpoint", "", "s3 url")
+	s3accessKeyID := flag.String("s3ID", "", "s3 access ID")
+	s3secretAccessKey := flag.String("s3Key", "", "s3 access Key")
+	s3useSSL := flag.Bool("s3ssl", false, "use ssl for s3")
+	projectID := flag.String("GCproject", "", "gcloud projectID")
+	GSbucketName := flag.String("GCbucketName", "", "gcloud bucket name")
+	excludeBucket := flag.String("exclude", "", "comma separated s3 bucket names to exclude from process")
+	tmpdir := flag.String("tmpdir", "./tmp/", "tmpdir")
+
+	flag.Parse()
+
+	excludeBuckets := make(map[string]bool)
+	for _, bucket := range strings.Split(*excludeBucket, ",") {
+		excludeBuckets[bucket] = true
+	}
+
+	if *projectID == "" || *s3endpoint == "" || *GSbucketName == "" {
+		log.Fatalf("GCproject, GSbucketName, s3endpoint variables must be set.\n")
+	}
+
+	var queueCapacity int32 = 1000
+	count := 0
+
+	Rclient := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	_, err := Rclient.Ping().Result()
+	if err != nil {
+		log.Print(err)
+	}
+
+	runtime.GOMAXPROCS(runtime.NumCPU())
+
+	workPool := workpool.New(runtime.NumCPU(), queueCapacity)
+
+	ctx := context.Background()
+
+	// Creates a GCClient.
+	GCClient, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	// Initialize minio client object.
+	s3Client, err := minio.New(*s3endpoint, *s3accessKeyID, *s3secretAccessKey, *s3useSSL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	s3buckets, err := s3Client.ListBuckets()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for _, s3bucket := range s3buckets {
+		if excludeBuckets[s3bucket.Name] == true {
+			continue
+		}
+		count = 0
+		log.Println("Create GS file map", s3bucket.Name)
+		err = getGSfileMap(Rclient, GCClient, ctx, *GSbucketName, s3bucket.Name+"/", "")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		doneCh := make(chan struct{})
+		defer close(doneCh)
+		log.Println("Compare GS file map to s3 files", s3bucket.Name)
+		for s3file := range s3Client.ListObjects(s3bucket.Name, "", true, doneCh) {
+			if s3file.Err != nil {
+				log.Println(s3file.Err)
+			}
+
+			GSmd5, err := Rclient.Get(s3bucket.Name + "/" + s3file.Key).Result()
+			if GSmd5 != strings.Split(s3file.ETag, "\"")[1] || err == redis.Nil {
+				work := MyWork{
+					s3fileName:   s3file.Key,
+					GCbucket:     *GSbucketName,
+					s3bucketname: s3bucket.Name,
+					ctx:          ctx,
+					s3Client:     s3Client,
+					GCClient:     GCClient,
+					WP:           workPool,
+					filename:     *tmpdir + strings.Split(s3file.ETag, "\"")[1],
+					count:        &count,
+				}
+				for queueCapacity-10 < workPool.QueuedWork() {
+					time.Sleep(100 * time.Millisecond)
+				}
+				if err := workPool.PostWork("routine", &work); err != nil {
+					log.Printf("ERROR: %s\n", err)
+				}
+				_ = Rclient.Del(s3bucket.Name + "/" + s3file.Key)
+			} else if err != nil {
+				log.Print(err)
+			} else {
+				_ = Rclient.Del(s3bucket.Name + "/" + s3file.Key)
+			}
+		}
+	}
+}
+
 func (mw *MyWork) DoWork(workRoutine int) {
 	//open s3 file
 	s3reader, err := mw.s3Client.GetObject(mw.s3bucketname, mw.s3fileName, minio.GetObjectOptions{})
@@ -115,105 +219,4 @@ func getGSfileMap(Rclient *redis.Client, client *storage.Client, ctx context.Con
 		}
 	}
 	return nil
-}
-
-func main() {
-	s3endpoint := flag.String("s3endpoint", "", "s3 url")
-	s3accessKeyID := flag.String("s3ID", "", "s3 access ID")
-	s3secretAccessKey := flag.String("s3Key", "", "s3 access Key")
-	s3useSSL := flag.Bool("s3ssl", false, "use ssl for s3")
-	projectID := flag.String("GCproject", "", "gcloud projectID")
-	GSbucketName := flag.String("GCbucketName", "", "gcloud bucket name")
-	excludeBucket := flag.String("exclude", "", "comma separated s3 bucket names to exclude from process")
-	tmpdir := flag.String("tmpdir", "./tmp/", "tmpdir")
-
-	flag.Parse()
-
-	excludeBuckets := make(map[string]bool)
-	for _, bucket := range strings.Split(*excludeBucket, ",") {
-		excludeBuckets[bucket] = true
-	}
-
-	if *projectID == "" || *s3endpoint == "" || *GSbucketName == "" {
-		log.Fatalf("GCproject, GSbucketName, s3endpoint variables must be set.\n")
-	}
-
-	var queueCapacity int32 = 1000
-	count := 0
-
-	Rclient := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "", // no password set
-		DB:       0,  // use default DB
-	})
-	_, err := Rclient.Ping().Result()
-	if err != nil {
-		log.Print(err)
-	}
-
-	runtime.GOMAXPROCS(runtime.NumCPU())
-
-	workPool := workpool.New(runtime.NumCPU(), queueCapacity)
-
-	ctx := context.Background()
-
-	// Creates a GCClient.
-	GCClient, err := storage.NewClient(ctx)
-	if err != nil {
-		log.Fatalf("Failed to create client: %v", err)
-	}
-
-	// Initialize minio client object.
-	s3Client, err := minio.New(*s3endpoint, *s3accessKeyID, *s3secretAccessKey, *s3useSSL)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	s3buckets, err := s3Client.ListBuckets()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	for _, s3bucket := range s3buckets {
-		if excludeBuckets[s3bucket.Name] == true {
-			continue
-		}
-		count = 0
-		log.Println("Create GS file map", s3bucket.Name)
-		err = getGSfileMap(Rclient, GCClient, ctx, *GSbucketName, s3bucket.Name+"/", "")
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		doneCh := make(chan struct{})
-		defer close(doneCh)
-		log.Println("Compare GS file map to s3 files", s3bucket.Name)
-		for s3file := range s3Client.ListObjects(s3bucket.Name, "", true, doneCh) {
-			if s3file.Err != nil {
-				log.Println(s3file.Err)
-			}
-
-			GSmd5, err := Rclient.Get("key2").Result()
-			if GSmd5 != strings.Split(s3file.ETag, "\"")[1] || err == redis.Nil {
-				work := MyWork{
-					s3fileName:   s3file.Key,
-					GCbucket:     *GSbucketName,
-					s3bucketname: s3bucket.Name,
-					ctx:          ctx,
-					s3Client:     s3Client,
-					GCClient:     GCClient,
-					WP:           workPool,
-					filename:     *tmpdir + strings.Split(s3file.ETag, "\"")[1],
-					count:        &count,
-				}
-				for queueCapacity-10 < workPool.QueuedWork() {
-					time.Sleep(100 * time.Millisecond)
-				}
-				if err := workPool.PostWork("routine", &work); err != nil {
-					log.Printf("ERROR: %s\n", err)
-				}
-			} else if err != nil {
-				log.Print(err)
-			}
-		}
-	}
 }
